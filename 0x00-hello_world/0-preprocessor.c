a28c00ba94b221a2a1cf8ac680caa4f4979ce15f
fadhilahbadmus <fadhilahbadmus734@gmaul.com>
2023-05-24 13:29:36 -0700

    task 0

--- 0x00-hello_world/0-preprocessor.c
+++ 0x00-hello_world/0-preprocessor.c
@@ -1,12 +1,2 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	printf("CFILE=main.c\n");
-	return (0);
-}
+#!/bin/bash
+gcc -E $CFILE -o c
